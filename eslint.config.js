2aef68fdad420e18dec9dcf91d4f2babada9a24f
Wilk9 <wilco@clcinternational.org>
2025-02-10 22:38:07 +0100

    Add eslint-plugin-react-compiler and update GameGrid component for improved image offset calculation

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactCompiler from "eslint-plugin-react-compiler"
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -7,17 +8,19 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'plugin:react-hooks/recommended' ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      'eslint-plugin-react-compiler': reactCompiler,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      'react-compiler/react-compiler': "error",
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
